ba2ddb7770bdaf7d0ee29abccdbcd17a910944a9
sanyal10 <100038306+sanyal10@users.noreply.github.com>
2023-03-07 21:34:29 +0530

    Add files via upload

---
+++ attendance-registration.js
@@ -0,0 +1,32 @@
+// Select the form element and add an event listener for submit
+// Select the form element and add an event listener for submit
+const form = document.getElementById("attendanceForm");
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent form submission
+
+  // Get the input values from the form
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const date = new Date().toLocaleDateString();
+
+  // Create a new row in the attendance table
+  const table = document.getElementById("attendanceTable");
+  const row = table.insertRow(-1);
+  const nameCell = row.insertCell(0);
+  const emailCell = row.insertCell(1);
+  const dateCell = row.insertCell(2);
+
+  // Set the values of the new row cells
+  nameCell.innerHTML = name;
+  emailCell.innerHTML = email;
+  dateCell.innerHTML = date;
+
+  // Reset the form
+  form.reset();
+
+  // Copy the table contents to the clipboard
+  const tableContent = table.outerHTML;
+  navigator.clipboard.writeText(tableContent)
+    .then(() => console.log('Table contents copied to clipboard'))
+    .catch(err => console.error('Failed to copy table contents: ', err));
+});
